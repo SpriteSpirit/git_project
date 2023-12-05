989bf3b1e40839bf93728b3974cec58ad73c65b3
Angelina Khalueva <sprite.spirit.t.me@gmail.com>
2023-12-05 12:46:10 +0300

    Добавила main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+print("рello дinux!")
+=======
+>>>>>>> parent of dbed7c2 (Добавила print() в main.py)
+=======
+>>>>>>> parent of dbed7c2 (Добавила print() в main.py)
+=======
+>>>>>>> parent of dbed7c2 (Добавила print() в main.py)
+=======
+print("Hello Linux!")
+>>>>>>> 36aa1b6 (main.py)
+=======
+>>>>>>> parent of dbed7c2 (Добавила print() в main.py)
+=======
+>>>>>>> parent of dbed7c2 (Добавила print() в main.py)
+=======
+>>>>>>> parent of dbed7c2 (Добавила print() в main.py)
